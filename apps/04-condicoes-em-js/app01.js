71e7aff1700141f4343942a55b2adc89b6f38f81
ChBM <solucao.erp@gmail.com>
2020-11-16 22:04:57 -0300

    Commit Default

--- apps/04-condicoes-em-js/app01.js
+++ apps/04-condicoes-em-js/app01.js
@@ -1,6 +1,12 @@
-console.log('O console funcionou corretamente!')
-
 var vel = 60.5
-console.log(`A vecolidade do seu carro é de [${vel} Km/h]`)
 
-console.log(`Dirija sempre usando cinto de segurança!`)
+console.log(`A vecolidade do seu carro é de [${vel} Km/h]!`)
+
+/*
+    Exemplo de Condição Simples
+*/
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de vecolidade permitido. MULTADO!`)
+}
+
+console.log(`Dirija-se sempre usando cinto de segurança!`)
---
+++ apps/04-condicoes-em-js/app02.js
@@ -0,0 +1,13 @@
+//var pais = 'Brasil'
+var pais = 'EUA'
+
+//console.log(`Vivendo em [${pais}]!`)
+
+/*
+    Exemplo de Condição Composta
+*/
+if (pais == 'Brasil') {
+    console.log(`Você é do [${pais}].`)
+} else {
+    console.log(`Você é Estrangeiro!`)
+}
